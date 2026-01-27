9108b0e96c0cd1a564eb3e73f633799566d7975f
Kshitiz  Yadav <97891909+YadavKshitiz@users.noreply.github.com>
2026-01-27 22:04:24 +0530

    Time: 0 ms (100%), Space: 43 MB (72.25%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,110 +1,110 @@
-// class Solution {
-//     public List<Integer> spiralOrder(int[][] matrix) {
-//         int m = matrix.length;
-//         int n = matrix[0].length;
-//         List<Integer> result = new ArrayList<>();
-//         int count = 0;
-//         boolean bool = false;
-//         int p = -1, k = n, l = -1, o = m;
-//         int i = 0, j = 0;
-//         while (count < m * n) {
-//             while (j < k) {
-//                 result.add(matrix[i][j]);
-//                 j++;
-//                 count++;
-//                 bool = true;
-//             }
-//             if (bool) {
-//                 p++;
-//                 j--;
-//                 i++;
-//                 bool = false;
-//             }
-//             if (count == m * n) {
-//                 return result;
-//             }
-//             while (i < o) {
-//                 result.add(matrix[i][j]);
-//                 i++;
-//                 count++;
-//                 bool = true;
-//             }
-//             if (bool) {
-//                 k--;
-//                 i--;
-//                 j--;
-//                 bool = false;
-//             }
-//             if (count == m * n) {
-//                 return result;
-//             }
-
-//             while (j > l) {
-//                 result.add(matrix[i][j]);
-//                 j--;
-//                 count++;
-//                 bool = true;
-//             }
-//             if (bool) {
-//                 o--;
-//                 j++;
-//                 i--;
-//                 bool = false;
-//             }
-//             if (count == m * n) {
-//                 return result;
-//             }
-
-//             while (i > p) {
-//                 result.add(matrix[i][j]);
-//                 count++;
-//                 i--;
-//                 bool = true;
-//             }
-//             if (bool) {
-//                 l++;
-//                 i++;
-//                 j++;
-//                 bool = false;
-//             }
-//             if (count == m * n) {
-//                 return result;
-//             }
-
-//         }
-//         return result;
-//     }
-// }
-
-//optimal
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
         List<Integer> result = new ArrayList<>();
-        if (matrix == null || matrix.length == 0) return result;
-        int top = 0, bottom = matrix.length - 1;
-        int left = 0, right = matrix[0].length - 1;
-        while (top <= bottom && left <= right) {
-            for (int j = left; j <= right; j++) {
-                result.add(matrix[top][j]);
+        int count = 0;
+        boolean bool = false;
+        int p = -1, k = n, l = -1, o = m;
+        int i = 0, j = 0;
+        while (count < m * n) {
+            while (j < k) {
+                result.add(matrix[i][j]);
+                j++;
+                count++;
+                bool = true;
             }
-            top++;
-            for (int i = top; i <= bottom; i++) {
-                result.add(matrix[i][right]);
+            if (bool) {
+                p++;
+                j--;
+                i++;
+                bool = false;
             }
-            right--;
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    result.add(matrix[bottom][j]);
-                }
-                bottom--;
+            if (count == m * n) {
+                return result;
             }
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    result.add(matrix[i][left]);
-                }
-                left++;
+            while (i < o) {
+                result.add(matrix[i][j]);
+                i++;
+                count++;
+                bool = true;
             }
+            if (bool) {
+                k--;
+                i--;
+                j--;
+                bool = false;
+            }
+            if (count == m * n) {
+                return result;
+            }
+
+            while (j > l) {
+                result.add(matrix[i][j]);
+                j--;
+                count++;
+                bool = true;
+            }
+            if (bool) {
+                o--;
+                j++;
+                i--;
+                bool = false;
+            }
+            if (count == m * n) {
+                return result;
+            }
+
+            while (i > p) {
+                result.add(matrix[i][j]);
+                count++;
+                i--;
+                bool = true;
+            }
+            if (bool) {
+                l++;
+                i++;
+                j++;
+                bool = false;
+            }
+            if (count == m * n) {
+                return result;
+            }
+
         }
         return result;
     }
 }
+
+//optimal
+// class Solution {
+//     public List<Integer> spiralOrder(int[][] matrix) {
+//         List<Integer> result = new ArrayList<>();
+//         if (matrix == null || matrix.length == 0) return result;
+//         int top = 0, bottom = matrix.length - 1;
+//         int left = 0, right = matrix[0].length - 1;
+//         while (top <= bottom && left <= right) {
+//             for (int j = left; j <= right; j++) {
+//                 result.add(matrix[top][j]);
+//             }
+//             top++;
+//             for (int i = top; i <= bottom; i++) {
+//                 result.add(matrix[i][right]);
+//             }
+//             right--;
+//             if (top <= bottom) {
+//                 for (int j = right; j >= left; j--) {
+//                     result.add(matrix[bottom][j]);
+//                 }
+//                 bottom--;
+//             }
+//             if (left <= right) {
+//                 for (int i = bottom; i >= top; i--) {
+//                     result.add(matrix[i][left]);
+//                 }
+//                 left++;
+//             }
+//         }
+//         return result;
+//     }
+// }

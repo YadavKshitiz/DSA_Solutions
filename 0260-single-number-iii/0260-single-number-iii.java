3fd8857bbc29769d042697276557e72c1811aec6
Kshitiz  Yadav <97891909+YadavKshitiz@users.noreply.github.com>
2026-04-06 22:02:37 +0530

    Time: 1 ms (100%), Space: 47.9 MB (68.01%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+
+        int setBit = res & (-res);
+        int []arr=new int[2];
+        for (int num : nums) {
+            if ((num & setBit) != 0) {
+                arr[0] ^= num;
+            } else {
+                arr[1] ^= num;
+            }
+        }
+        return arr;
+    }
+}

2e09fa769331cb8a9ef002a3448ba73a1a458d72
Kshitiz  Yadav <97891909+YadavKshitiz@users.noreply.github.com>
2026-04-06 19:45:34 +0530

    Time: 9 ms (99.03%), Space: 50.2 MB (65.25%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -5,9 +5,9 @@ class Solution {
             left = Math.max(left, w);
             right += w;
         }
-        
+
         while (left < right) {
-            int mid = (left + right) / 2, need = 1, current = 0;
+            int mid = left + (right - left) / 2, need = 1, current = 0;
             for (int w : weights) {
                 if (current + w > mid) {
                     need++;

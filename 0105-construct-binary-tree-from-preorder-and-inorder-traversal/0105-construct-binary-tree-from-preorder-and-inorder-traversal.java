ee4386cb39a6eedc0f7c9725bedbf090f390a790
Kshitiz  Yadav <97891909+YadavKshitiz@users.noreply.github.com>
2026-06-14 15:57:17 +0530

    Time: 2 ms (63.31%), Space: 46.7 MB (14.04%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,31 +14,30 @@
  * }
  */
 class Solution {
-    int start=0;
-    Map<Integer,Integer> map=new HashMap<>();
-    
+    int start = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int leftBound=0, rightBound=inorder.length-1;
-        int i=0;
-        while(i<inorder.length){
-            map.put(inorder[i],i);
+        int leftBound = 0, rightBound = inorder.length - 1;
+        int i = 0;
+        while (i < inorder.length) {
+            map.put(inorder[i], i);
             i++;
         }
-        return build(preorder, leftBound,rightBound);
+        return build(preorder, leftBound, rightBound);
 
     }
 
-    public TreeNode build(int[] preorder, int leftBound, int rightBound){
-        if(leftBound>rightBound){
+    public TreeNode build(int[] preorder, int leftBound, int rightBound) {
+        if (leftBound > rightBound) {
             return null;
         }
 
         TreeNode root;
-        
-            root=new TreeNode(preorder[start++]);
-            int rootIndex=map.get(root.val);
-            root.left=build(preorder,leftBound,rootIndex-1);
-            root.right=build(preorder,rootIndex+1,rightBound);
+        root = new TreeNode(preorder[start++]);
+        int rootIndex = map.get(root.val);
+        root.left = build(preorder, leftBound, rootIndex - 1);
+        root.right = build(preorder, rootIndex + 1, rightBound);
         return root;
     }
 }

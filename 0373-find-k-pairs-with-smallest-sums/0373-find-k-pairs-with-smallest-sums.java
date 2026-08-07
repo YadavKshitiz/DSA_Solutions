997c995aac58e00bc1bd26577a3d211397df30ec
Kshitiz  Yadav <97891909+YadavKshitiz@users.noreply.github.com>
2026-08-07 20:05:46 +0530

    Time: 41 ms (33.8%), Space: 155.8 MB (5.67%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        for(int i=0;i<nums1.length;i++){
-            heap.offer(new int[]{nums1[i]+nums2[0],i,0});
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < nums1.length; i++) {
+            heap.offer(new int[] { nums1[i] + nums2[0], i, 0 });
         }
-        List<List<Integer>> result=new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        while(!heap.isEmpty()&&result.size()!=k){
-            int[] current =heap.poll();
-            List<Integer> list=new ArrayList<>();
-            list.add(nums1[current[1]]);
-            list.add(nums2[current[2]]);
+        while (!heap.isEmpty() && k>0) {
+            int[] current = heap.poll();
+            List<Integer> list = new ArrayList<>();
+            int i = current[1];
+            int j = current[2];
+            list.add(nums1[i]);
+            list.add(nums2[j]);
             result.add(list);
 
-            if(current[2]+1<nums2.length){
-               heap.offer(new int[]{
-                nums1[current[1]]+nums2[current[2]+1],
-                current[1],
-                current[2]+1
-               }
-               );
+            if (j + 1 < nums2.length) {
+                heap.offer(new int[] {
+                        nums1[i] + nums2[j + 1],
+                        i,
+                        j + 1
+                });
             }
+            k--;
         }
         return result;
     }
